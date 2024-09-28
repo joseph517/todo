184a336583671b609a690f5c8bdad3b70fa8b871
joseph517 <alvarovergara1993@hotmail.com>
2024-09-27 20:05:29 -0500

    remove angular-material

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TasksRoutingModule } from './tasks-routing.module';
-import { AngularMaterialModule } from '../angular-material/angular-material.module';
 import { CreateTaskComponent } from './components/create-task/create-task.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
@@ -12,7 +11,6 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
   ],
   imports: [
     CommonModule,
-    AngularMaterialModule,
     TasksRoutingModule,
     FormsModule,
     ReactiveFormsModule
